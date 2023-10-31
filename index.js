af739764c794ebcda858df73976692e3421cf1ee
Atlas_Kaplan <venatic.kaplan@gmail.com>
2023-10-31 10:37:16 -0600

    themes completed

---
+++ index.js
@@ -0,0 +1,21 @@
+
+var sol = document.getElementById('temaSwitch1');
+var luna = document.getElementById('temaSwitch2');
+var body = document.querySelector('.body');
+
+sol.addEventListener('click', function () {
+
+    body.classList.toggle('color');
+});
+luna.addEventListener('click', function () {
+
+    body.classList.toggle('color');
+});
+
+function cambiarTema() {
+
+
+  // Toggle classes
+    sol.classList.toggle('hidden');
+    luna.classList.toggle('hidden');
+}
