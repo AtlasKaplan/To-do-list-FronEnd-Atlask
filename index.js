4319ecf21feb21cf516c1bffe741c4a84a814a45
Atlas_Kaplan <venatic.kaplan@gmail.com>
2023-11-06 00:20:22 -0600

    contador

--- index.js
+++ index.js
@@ -6,30 +6,69 @@ var input = document.querySelector('.input-task');
 var addBtn = document.querySelector('.new-todo .add-btn');
 var container = document.querySelector('.container');
 
-// Cargar tareas almacenadas al cargar la página
+// Cargar tema almacenado al cargar la página
 window.addEventListener('load', function () {
-    // Llamar a loadTasks y después a contarTaskDivs cuando la carga esté completa
+    // Llamar a cargarTema y después a cargarTareas y contarTaskDivs cuando la carga esté completa
+    cargarTema();
     loadTasks().then(function () {
         contarTaskDivs();
     });
+    guardarTema(); // Agregar esta línea para asegurarse de que el tema se aplique correctamente
+
+    // Verificar y aplicar el estado del fondo al cargar la página
+    if ((body.classList.contains('color') && !sol.classList.contains('hidden')) ||
+        (!body.classList.contains('color') && !luna.classList.contains('hidden'))) {
+        body.classList.toggle('color');
+    }
 });
 
 // Cambiar tema
 sol.addEventListener('click', function () {
     body.classList.toggle('color');
+    cambiarTemaSol();
+    guardarTema();
 });
 luna.addEventListener('click', function () {
     body.classList.toggle('color');
+    cambiarTemaLuna();
+    guardarTema();
 });
 
+// Función para cambiar el tema al modo sol
 function cambiarTemaSol() {
     sol.classList.add('hidden');
     luna.classList.remove('hidden');
+    body.classList.remove('color');
+
+    // Verificar y aplicar el estado del fondo
+    if (body.classList.contains('color')) {
+        body.classList.toggle('color');
+    }
 }
 
+// Función para cambiar el tema al modo luna
 function cambiarTemaLuna() {
     luna.classList.add('hidden');
     sol.classList.remove('hidden');
+    body.classList.add('color');
+
+    // Verificar y aplicar el estado del fondo
+    if (!body.classList.contains('color')) {
+        body.classList.toggle('color');
+    }
+}
+// Función para guardar el estado del tema en localStorage
+function guardarTema() {
+    var tema = body.classList.contains('color') ? 'color' : '';
+    localStorage.setItem('tema', tema);
+}
+
+// Función para cargar el estado del tema desde localStorage
+function cargarTema() {
+    var storedTema = localStorage.getItem('tema');
+    if (storedTema) {
+        body.classList.toggle('color', storedTema === 'color');
+    }
 }
 function contarTaskDivs() {
     // Obtén la cantidad de elementos con la clase 'task-div' que no tienen la clase 'active'
@@ -39,8 +78,7 @@ function contarTaskDivs() {
     document.querySelector('.total-task').textContent = totalTaskDivs;
 }
 
-// Llama a la función al cargar la página para inicializar el contador
-contarTaskDivs();
+
 
 // Función para agregar una tarea
 function addTask() {
@@ -129,39 +167,39 @@ function loadTasks() {
 function toggleActiveState(element) {
       // Alternar la clase "active" en el contenedor al hacer clic
     element.classList.toggle('active');
+    
     contarTaskDivs();
+    saveTasks();
     }
 
 // Asociar funciones a eventos
 addBtn.addEventListener('click', function () {
     addTask();
-    contarTaskDivs();  // Llama a contarTaskDivs después de agregar una tarea
+
+    contarTaskDivs();
+    saveTasks();
 });
 
 input.addEventListener('keydown', function (event) {
     if (event.key === 'Enter') {
         addTask();
-        contarTaskDivs();  // Llama a contarTaskDivs después de agregar una tarea
+
+        contarTaskDivs();
+        saveTasks();
     }
 });
 
 
 
 // boton clear
-
 var clear = document.getElementById('clear');
-
-// Agregar el event listener al botón clear
-clear.addEventListener('click', function() {
-  // Obtener todos los elementos con la clase 'task-div' y 'active'
+clear.addEventListener('click', function () {
     var taskDivs = document.querySelectorAll('.task-div.active');
-
-  // Iterar sobre los elementos y eliminar cada uno
-    taskDivs.forEach(function(taskDiv) {
-    taskDiv.remove();
-    saveTasks();
+    taskDivs.forEach(function (taskDiv) {
+        taskDiv.remove();
     });
+    // Llama a contarTaskDivs después de eliminar tareas y guarda el estado actualizado
     contarTaskDivs();
+    saveTasks();
 });
-
 
