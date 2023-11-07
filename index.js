25b0912ec2cfba28f55856ca361254c31090742d
Atlas_Kaplan <venatic.kaplan@gmail.com>
2023-11-07 09:50:27 -0600

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ var body = document.querySelector('.body');
 var input = document.querySelector('.input-task');
 var addBtn = document.querySelector('.new-todo .add-btn');
 var container = document.querySelector('.container');
+var clear = document.getElementById('clear');
 
 // Cargar tema almacenado al cargar la página
 window.addEventListener('load', function () {
@@ -13,15 +14,10 @@ window.addEventListener('load', function () {
 });
 
 // Cambiar tema
-sol.addEventListener('click', function () {
-    cambiarTema();
-});
+sol.addEventListener('click', cambiarTema);
+luna.addEventListener('click', cambiarTema);
 
-luna.addEventListener('click', function () {
-    cambiarTema();
-});
-
-// Función para cambiar el tema
+// Funciones relacionadas con el tema
 function cambiarTema() {
     sol.classList.toggle('hidden');
     luna.classList.toggle('hidden');
@@ -32,14 +28,11 @@ function cambiarTema() {
     saveTasks();
 }
 
-// Función para guardar el estado del tema en localStorage
 function guardarTema() {
     var tema = body.classList.contains('color') ? 'color' : '';
     localStorage.setItem('tema', tema);
 }
 
-// Función para cargar el estado del tema desde localStorage
-// Función para cargar el estado del tema desde localStorage
 function cargarTema() {
     var storedTema = localStorage.getItem('tema');
     if (storedTema === 'color') {
@@ -53,43 +46,32 @@ function cargarTema() {
     }
 }
 
-
+// Funciones relacionadas con las tareas
 function contarTaskDivs() {
-    // Obtén la cantidad de elementos con la clase 'task-div' que no tienen la clase 'active'
     var totalTaskDivs = document.querySelectorAll('.task-div:not(.active)').length;
-
-    // Actualiza el contenido del elemento con la clase 'total-task'
     document.querySelector('.total-task').textContent = totalTaskDivs;
 }
 
-
-
-// Función para agregar una tarea
 function addTask() {
     var value = input.value.trim();
-
     if (value !== '' && value !== '0') {
         var capitalizedValue = value.charAt(0).toUpperCase() + value.slice(1);
         createTaskElement(capitalizedValue);
         input.value = '';
-        contarTaskDivs()
+        contarTaskDivs();
         saveTasks(); // Guardar la lista después de agregar una tarea
     }
 }
 
-// Función para crear un elemento de tarea
 function createTaskElement(taskText) {
     var newDiv = document.createElement('div');
-    newDiv.classList.add('task-div', 'brorder-bottom');
-
+    newDiv.classList.add('task-div', 'border-bottom');
     var checkIcon = document.createElement('img');
     checkIcon.src = 'images/icon-check.svg';
     checkIcon.alt = 'check';
-
     var taskTextElement = document.createElement('p');
     taskTextElement.textContent = taskText;
 
-    // Agrega el evento onclick al elemento taskTextElement
     taskTextElement.onclick = function () {
         toggleActiveState(newDiv);
     };
@@ -99,14 +81,12 @@ function createTaskElement(taskText) {
     crossIcon.src = 'images/icon-cross.svg';
     crossIcon.alt = 'cross';
 
-    // Agrega el evento onclick al elemento crossIcon
     crossIcon.addEventListener('click', function () {
         newDiv.remove();
         contarTaskDivs();
         saveTasks(); // Guardar la lista después de eliminar una tarea
     });
 
-    // Agrega el evento onclick al elemento checkIcon
     checkIcon.onclick = function () {
         toggleActiveState(newDiv);
     };
@@ -118,11 +98,10 @@ function createTaskElement(taskText) {
     container.appendChild(newDiv);
 }
 
-// Función para guardar las tareas en localStorage
 function saveTasks() {
     var tasks = [];
     var taskDivs = document.querySelectorAll('.task-div');
-
+    
     taskDivs.forEach(function (taskDiv) {
         tasks.push({
             text: taskDiv.querySelector('p').textContent,
@@ -130,12 +109,10 @@ function saveTasks() {
         });
     });
 
-
     localStorage.setItem('tasks', JSON.stringify(tasks));
     guardarTema();
 }
 
-// Función para cargar las tareas desde localStorage
 function loadTasks() {
     var storedTasks = localStorage.getItem('tasks');
 
@@ -145,14 +122,12 @@ function loadTasks() {
         tasks.forEach(function (task) {
             createTaskElement(task.text);
 
-            // Aplicar el estado active si es necesario
             var lastTaskDiv = document.querySelector('.task-div:last-child');
             if (task.active) {
                 toggleActiveState(lastTaskDiv);
             }
         });
 
-        // Asegúrate de contar las tareas después de cargarlas
         contarTaskDivs();
     }
 }
@@ -166,7 +141,6 @@ function toggleActiveState(element) {
 // Asociar funciones a eventos
 addBtn.addEventListener('click', function () {
     addTask();
-
     contarTaskDivs();
     saveTasks();
 });
@@ -174,22 +148,17 @@ addBtn.addEventListener('click', function () {
 input.addEventListener('keydown', function (event) {
     if (event.key === 'Enter') {
         addTask();
-
         contarTaskDivs();
         saveTasks();
     }
 });
 
-
-
-// boton clear
-var clear = document.getElementById('clear');
 clear.addEventListener('click', function () {
     var taskDivs = document.querySelectorAll('.task-div.active');
     taskDivs.forEach(function (taskDiv) {
         taskDiv.remove();
     });
-    // Llama a contarTaskDivs después de eliminar tareas y guarda el estado actualizado
+
     contarTaskDivs();
     saveTasks();
 });
